051c0d576b41fed1e0be41393800904dc70e41b9
Pavel Znamenáček <pavel.znamenacek@gmail.com>
2021-01-05 01:00:41 +0100

    Adjusted SQL model to minimise differences generated while deploying  the version.

--- sources/main/DbSchema.Version/Feature/Audit/Structure/audit_event.sql
+++ sources/main/DbSchema.Version/Feature/Audit/Structure/audit_event.sql
@@ -3,15 +3,15 @@
       [id] int not null identity(0, 1)
         constraint [pk_schema_version_audit_event_id] primary key
     , [session_id] smallint not null
-        constraint [df_schema_version_audit_event_session_id] default @@spid
+        constraint [df_schema_version_audit_event_session_id] default (@@spid)
     , [when] datetime not null
-        constraint [df_schema_version_audit_event_when] default getutcdate()
+        constraint [df_schema_version_audit_event_when] default (getutcdate())
     , [user_login] nvarchar(128) not null
-        constraint [df_schema_version_audit_event_user_login] default system_user
+        constraint [df_schema_version_audit_event_user_login] default (suser_sname())
     , [user_identity] nvarchar(128) not null
-        constraint [df_schema_version_audit_event_user_identity] default current_user
+        constraint [df_schema_version_audit_event_user_identity] default (user_name())
     , [user_session] nvarchar(128) not null
-        constraint [df_schema_version_audit_event_user_session] default session_user
+        constraint [df_schema_version_audit_event_user_session] default (user_name())
     , [from] sysname not null
     , [message] nvarchar(2048) not null
 );
--- sources/main/DbSchema.Version/Feature/Step/Structure/step.sql
+++ sources/main/DbSchema.Version/Feature/Step/Structure/step.sql
@@ -4,9 +4,9 @@
     , [schema_name] [schema_version].[t_schema_name] not null
     , [application_name] [schema_version].[t_application_name] not null
     , [version] [schema_version].[t_version] not null
-    , [upgrade] bit not null constraint df_schema_version_step_upgrade default 1
+    , [upgrade] bit not null constraint df_schema_version_step_upgrade default ((1))
     , [sequence] smallint not null
     , [description] nvarchar(128) not null
     , [procedure] [sys].[sysname] not null
-    , [completed] bit not null constraint df_schema_version_step_completed default 0
+    , [completed] bit not null constraint df_schema_version_step_completed default ((0))
 );
